075c66fd301f048f7981978134299e2955da5ea5
frolovAlex2093 <75167740+frolovAlex2093@users.noreply.github.com>
2023-01-12 14:57:14 +0300

    Delete src/components/Loader directory

--- src/components/Loader/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { Box } from '@mui/material';
-
-export const Loader: React.FC = () => (
-    <Box display='flex' justifyContent='center'>
-      <Box className='lds-dual-ring' />
-    </Box>
-  );
