2650f7e6588f1d6a46f916cf7ce47301113fd646
frolovAlex2093 <75167740+frolovAlex2093@users.noreply.github.com>
2023-02-01 12:37:24 +0300

    Update upload.php

--- public/upload.php
+++ public/upload.php
@@ -1,3 +1,4 @@
 <?php
-	file_put_contents('file.txt', 'Ваш превосходный текст, который добавиться в уже существующий файл', FILE_APPEND);
+// 	file_put_contents('file.txt', 'Ваш превосходный текст, который добавиться в уже существующий файл', FILE_APPEND);
+	echo("<script>console.log('PHP: qq');</script>");
 ?>
