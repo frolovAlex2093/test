8fbdae604c4a13504611da459150f01d08e37d91
frolovAlex2093 <75167740+frolovAlex2093@users.noreply.github.com>
2023-06-30 13:22:09 +0300

    Update Third.tsx

--- src/pages/Third.tsx
+++ src/pages/Third.tsx
@@ -3107,7 +3107,7 @@ export const Third: React.FC = () => {
     });
     json.date = date.toISOString()
     await post2(json);
-    //console.log(JSON.stringify(json));
+    console.log(JSON.stringify(json));
     //alert(JSON.stringify(json));
 
   };
