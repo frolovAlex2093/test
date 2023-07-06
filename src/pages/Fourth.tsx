c1b6cf1e7feb1c2ec90fbf51b58ed97f0fafdc25
frolovAlex2093 <75167740+frolovAlex2093@users.noreply.github.com>
2023-07-06 10:15:41 +0300

    Update Fourth.tsx

--- src/pages/Fourth.tsx
+++ src/pages/Fourth.tsx
@@ -3284,9 +3284,25 @@ export const Fourth: React.FC = () => {
       return items;
     });
     json.date = date.toISOString();
-     await post2(json);
-    console.log(JSON.stringify(json));
-    // alert("ok");
+    //await post2(json);
+     console.log(JSON.stringify(json));
+      //alert("ok");
+	  let req = new XMLHttpRequest();
+
+      req.onreadystatechange = () => {
+        if (req.readyState === XMLHttpRequest.DONE) {
+          console.log(req.responseText);
+        }
+      };
+
+      req.open('POST', 'https://api.jsonbin.io/v3/b', true);
+      req.setRequestHeader('Content-Type', 'application/json');
+      req.setRequestHeader(
+        'X-Master-Key',
+        '$2b$10$cM7Z7Zy5ix9vNxMAxf8BLu7sgZggJXmdyyiQHeFICOtUC82IEKseu'
+      );
+      req.send(JSON.stringify(json));
+      console.log('ok');
   };
 
   // const post = async (object: Object) => {
@@ -3322,28 +3338,28 @@ export const Fourth: React.FC = () => {
 //      console.log('ok');
 //    };
 
-	const post2 = async (object: Object) => {
-  try {
-    const response = await fetch('https://api.jsonbin.io/v3/b', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Master-Key': '$2b$10$cM7Z7Zy5ix9vNxMAxf8BLu7sgZggJXmdyyiQHeFICOtUC82IEKseu'
-      },
-      body: JSON.stringify(object)
-    });
+// 	const post2 = async (object: Object) => {
+//   try {
+//     const response = await fetch('https://api.jsonbin.io/v3/b', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'X-Master-Key': '$2b$10$cM7Z7Zy5ix9vNxMAxf8BLu7sgZggJXmdyyiQHeFICOtUC82IEKseu'
+//       },
+//       body: JSON.stringify(object)
+//     });
 
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data);
-    } else {
-      console.error('Error:', response.status);
-    }
-  } catch (error) {
-    console.error('Error:', error);
-  }
-  console.log('ok');
-};
+//     if (response.ok) {
+//       const data = await response.json();
+//       console.log(data);
+//     } else {
+//       console.error('Error:', response.status);
+//     }
+//   } catch (error) {
+//     console.error('Error:', error);
+//   }
+//   console.log('ok');
+// };
 
 
   const insert = function insert(main_string: string, ins_string: string, pos: number): string {
