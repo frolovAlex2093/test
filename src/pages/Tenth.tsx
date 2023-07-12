1f132c488b7da9689163be1057e45812509b050b
frolovAlex2093 <75167740+frolovAlex2093@users.noreply.github.com>
2023-07-12 12:34:30 +0300

    Update Tenth.tsx

--- src/pages/Tenth.tsx
+++ src/pages/Tenth.tsx
@@ -3273,7 +3273,7 @@ export const Tenth: React.FC = () => {
           if (i.id === 159) {
             maker = i.value[0];
           }
-          if (i.name === 'ОГРН/ОКЮЛП(УНП)/ОКПО/Номер ГРЮЛ/БИН') {
+          if (i.name === 'ОГРН/ОКЮЛП(УНП)/ОКПО/Номер ГРЮЛ/БИН' && i.id === 198) {
             ogrn = i.value[0];
           }
           if (
