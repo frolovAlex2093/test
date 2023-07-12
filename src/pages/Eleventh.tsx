e94d82db88e0175d95616f9d2e70e3a7e36642fc
frolovAlex2093 <75167740+frolovAlex2093@users.noreply.github.com>
2023-07-12 12:37:58 +0300

    Update Eleventh.tsx

--- src/pages/Eleventh.tsx
+++ src/pages/Eleventh.tsx
@@ -3458,7 +3458,7 @@ export const Eleventh: React.FC = () => {
           if (i.id === 159) {
             maker = i.value[0];
           }
-          if (i.name === 'ОГРН/ОКЮЛП(УНП)/ОКПО/Номер ГРЮЛ/БИН') {
+          if (i.name === 'ОГРН/ОКЮЛП(УНП)/ОКПО/Номер ГРЮЛ/БИН' && i.id === 198) {
             ogrn = i.value[0];
           }
           if (
