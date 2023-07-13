53e3587eafbacc0455c15125dd9baa8dea226bd4
frolovAlex2093 <75167740+frolovAlex2093@users.noreply.github.com>
2023-07-13 10:25:39 +0300

    Update Tenth.tsx

--- src/pages/Tenth.tsx
+++ src/pages/Tenth.tsx
@@ -1260,7 +1260,7 @@ export const Tenth: React.FC = () => {
                 item.blockItem[z].value[0] !== '' &&
                 z !== 0
               ) {
-                str += `</trcdo:VehiclePowerTakeOffDetails></trcdo:TransmissionUnitDetails><trcdo:TransmissionUnitDetails><trsdo:VehicleUnitKindCode>20</trsdo:VehicleUnitKindCode><trsdo:VehicleComponentText>${item.blockItem[z].value[0]}</trsdo:VehicleComponentText>`;
+                str += `</trcdo:VehiclePowerTakeOffDetails></trcdo:TransmissionUnitDetails><trcdo:TransmissionUnitDetails><trsdo:VehicleUnitKindCode>20</trsdo:VehicleUnitKindCode><trcdo:VehiclePowerTakeOffDetails><trsdo:VehicleComponentText>${item.blockItem[z].value[0]}</trsdo:VehicleComponentText>`;
               }
             }
             str += '</trcdo:VehiclePowerTakeOffDetails></trcdo:TransmissionUnitDetails>';
