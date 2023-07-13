05d018612a4f4bf1493e15ab173cd79c3ca2169b
frolovAlex2093 <75167740+frolovAlex2093@users.noreply.github.com>
2023-07-13 10:26:03 +0300

    Update Third.tsx

--- src/pages/Third.tsx
+++ src/pages/Third.tsx
@@ -1171,7 +1171,7 @@ export const Third: React.FC = () => {
                 item.blockItem[z].value[0] !== '' &&
                 z !== 0
               ) {
-                str += `</trcdo:VehiclePowerTakeOffDetails></trcdo:TransmissionUnitDetails><trcdo:TransmissionUnitDetails><trsdo:VehicleUnitKindCode>20</trsdo:VehicleUnitKindCode><trsdo:VehicleComponentText>${item.blockItem[z].value[0]}</trsdo:VehicleComponentText>`;
+                str += `</trcdo:VehiclePowerTakeOffDetails></trcdo:TransmissionUnitDetails><trcdo:TransmissionUnitDetails><trsdo:VehicleUnitKindCode>20</trsdo:VehicleUnitKindCode><trcdo:VehiclePowerTakeOffDetails><trsdo:VehicleComponentText>${item.blockItem[z].value[0]}</trsdo:VehicleComponentText>`;
               }
             }
             str += '</trcdo:VehiclePowerTakeOffDetails></trcdo:TransmissionUnitDetails>';
