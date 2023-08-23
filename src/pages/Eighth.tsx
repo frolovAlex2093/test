e7fcf047516eaf9f6f923dba36dc3dffb935bf7a
frolovAlex2093 <75167740+frolovAlex2093@users.noreply.github.com>
2023-08-23 11:50:26 +0300

    Update Eighth.tsx

--- src/pages/Eighth.tsx
+++ src/pages/Eighth.tsx
@@ -3468,9 +3468,10 @@ export const Eighth: React.FC = () => {
     });
 
     if (
-      /^(.+|)<trcdo:VehicleManufacturerDetails>(\n|)(.+|)<ccdo:SubjectAddressDetails>(\n|)(.+|)<\/ccdo:SubjectAddressDetails>(\n|)(.+|)<\/trcdo:VehicleManufacturerDetails>(\n|)(.+|)$/.test(
-        str
-      ) ||
+      (str.includes('<trcdo:VehicleManufacturerDetails>') &&
+        str.includes('</trcdo:VehicleManufacturerDetails>') &&
+        str.includes('<ccdo:SubjectAddressDetails>') &&
+        str.includes('</ccdo:SubjectAddressDetails>')) ||
       str.length === 0
     ) {
       return { str1: str, b: true };
